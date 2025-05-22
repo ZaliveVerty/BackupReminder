5e31b29079d39ce3e777a4a5af0a24e4f87bda21
ZaliveVerty <tawlerwood90@gmail.com>
2025-05-22 14:47:48 +0300

    Commit

---
+++ BackupReminder/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace BackupReminder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.addButotn = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.index,
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.ControlDark;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(586, 344);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // index
+            // 
+            this.index.HeaderText = "Index";
+            this.index.Name = "index";
+            this.index.ReadOnly = true;
+            this.index.Visible = false;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column1.HeaderText = "Name";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Path";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column3.HeaderText = "Last backup";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 120;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(518, 368);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(80, 25);
+            this.closeButton.TabIndex = 1;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // addButotn
+            // 
+            this.addButotn.Location = new System.Drawing.Point(12, 368);
+            this.addButotn.Name = "addButotn";
+            this.addButotn.Size = new System.Drawing.Size(75, 25);
+            this.addButotn.TabIndex = 2;
+            this.addButotn.Text = "Add";
+            this.addButotn.UseVisualStyleBackColor = true;
+            this.addButotn.Click += new System.EventHandler(this.addButotn_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(94, 368);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 25);
+            this.removeButton.TabIndex = 3;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(196, 368);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 25);
+            this.updateButton.TabIndex = 4;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.DimGray;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(280, 377);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(77, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Open backups";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 403);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButotn);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Backup Reminder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button addButotn;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn index;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BackupReminder/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace BackupReminder
+{
+    public partial class Form1 : Form
+    {
+        public static String path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Backup Reminder";
+        public static List<List<String>> listOfFiles = new List<List<String>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //CHECK BASE
+            checkBase();
+
+            //REFRESH DATAGRIDVIEW
+            refresh_dataGridView();
+
+            //START TIMER
+            timer1.Start();
+        }
+
+        private static void setList() {
+            FileInfo[] files = new DirectoryInfo(path + @"\Storage").GetFiles();
+
+            listOfFiles.Clear();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                String[] array = readFile(files[i].FullName.ToString()).Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                listOfFiles.Add(new List<String> { files[i].FullName, array[0], array[1], array[2], array[3] });
+            }
+        }
+
+        public void refresh_dataGridView() {
+            //GET ALL FILES IN A LIST
+            setList();
+
+            dataGridView1.Rows.Clear();
+
+            for (int i = 0; i < listOfFiles.Count; i++) {
+                dataGridView1.Rows.Add(listOfFiles[i][0], listOfFiles[i][1], listOfFiles[i][2], listOfFiles[i][4]);
+            }
+        }
+
+        private static void checkBase() {
+            createDirectory(path + @"\Storage");
+        }
+
+        public static void createDirectory(String path) {
+            Directory.CreateDirectory(path);
+        }
+
+        public static void createFile(String path, String text)
+        {
+            using (var tw = new StreamWriter(path, false))
+            {
+                tw.WriteLine(text);
+            }
+        }
+
+        public static void deleteFile(String path) {
+            File.Delete(path);
+        }
+
+        public static String readFile(String path) {
+            return File.ReadAllText(path);
+        }
+
+        public String getCol(int index) {
+            return dataGridView1.SelectedRows[0].Cells[index].Value.ToString();
+        }
+
+        private void addButotn_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            Form2.form = this;
+            form2.ShowDialog();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to remove this?\t\t", "Remove", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                deleteFile(getCol(0));
+                refresh_dataGridView();
+            }
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            String[] array = readFile(getCol(0)).Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            createFile(getCol(0), array[0] + "\n" + array[1] + "\n" + array[2] + "\n" + DateTime.Now.ToString("yyyy-MM-dd HH:mm"));
+
+            refresh_dataGridView();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            String path1 = listOfFiles[dataGridView1.SelectedRows[0].Index][2];
+            String path2 = listOfFiles[dataGridView1.SelectedRows[0].Index][3];
+
+            Process.Start("explorer.exe", "/select, \"" + path1 + "\"");
+            Process.Start("explorer.exe", "/select, \"" + path2 + "\"");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                removeButton.Enabled = true;
+                updateButton.Enabled = true;
+                linkLabel1.Enabled = true;
+            }
+            else {
+                removeButton.Enabled = false;
+                updateButton.Enabled = false;
+                linkLabel1.Enabled = false;
+            }
+        }
+    }
+}
